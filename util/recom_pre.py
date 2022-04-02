24b2f7af204502bd1c2f366495b45703abfa156b
Jaehyun Jeon <chris990126@gmail.com>
2022-04-03 00:46:50 +0900

    transforms more options

--- util/recom_pre.py
+++ util/recom_pre.py
@@ -15,20 +15,25 @@ category_name['아우터'] = 'outwear'
 def get_recom_transform():
     data_transforms = {
         'train': transforms.Compose([
-            transforms.RandomResizedCrop(224),
-            transforms.RandomHorizontalFlip(),
+            transforms.Resize((224, 224)),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2), # 이미지 지터링(밝기, 대조, 채비, 색조)
+            transforms.RandomHorizontalFlip(p = 0.5), # p확률로 이미지 좌우반전
+            transforms.RandomVerticalFlip(p = 0.5), # p확률로 상하반전
         ]),
         'val': transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
+            transforms.Resize((224, 224)),
             transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2), # 이미지 지터링(밝기, 대조, 채비, 색조)
+            transforms.RandomHorizontalFlip(p = 0.5), # p확률로 이미지 좌우반전
+            transforms.RandomVerticalFlip(p = 0.5), # p확률로 상하반전
         ]),
         'candidate_emb': transforms.Compose([
             transforms.Resize((224, 224)),
-            transforms.ToTensor()
+            transforms.ToTensor(),
+            transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2), # 이미지 지터링(밝기, 대조, 채비, 색조)
+            transforms.RandomHorizontalFlip(p = 0.5), # p확률로 이미지 좌우반전
+            transforms.RandomVerticalFlip(p = 0.5), # p확률로 상하반전
         ]),
     }
     return data_transforms
